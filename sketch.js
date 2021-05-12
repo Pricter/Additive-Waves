fefcd7a625af54db071eec20b38f4191239d2eea
Pricter <67459196+Pricter@users.noreply.github.com>
2021-05-12 09:59:42 +0530

    Added Code

---
+++ sketch.js
@@ -0,0 +1,41 @@
+class Wave {
+    constructor(amp, period, phase) {
+        this.amplitude = amp;
+        this.period = period;
+        this.phase = phase;
+    }
+
+    calculate(x) {
+        return sin(this.phase + TWO_PI * x / this.period) * this.amplitude;
+    }
+
+    update() {
+        this.phase += 0.05
+    }
+}
+
+let waves = [];
+
+function setup() {
+    createCanvas(600, 400);
+    for(let i = 0; i < 5; i++) {
+        waves[i] = new Wave(random(20, 80), random(100, 600), random(0, TWO_PI));
+    }
+}
+
+function draw() {
+    background(0);
+
+    for(let x = 0; x < width; x += 5) {
+        let y = 0;
+        for(let wave of waves) {
+            y += wave.calculate(x);
+        }
+        noStroke();
+        ellipse(x, y + height / 2, 5);
+    }
+
+    for(let wave of waves) {
+        wave.update();
+    }
+}
